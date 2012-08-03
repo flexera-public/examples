54032ed176030184d816091be835a99563f8f1b6
Ravi Bhure <ravibhure@gmail.com>
2012-08-03 11:58:46 +0000

    added serverid page

--- app_test/settings.py
+++ app_test/settings.py
@@ -123,7 +123,6 @@ INSTALLED_APPS = (
     'django.contrib.admin',
     # Uncomment the next line to enable admin documentation:
     # 'django.contrib.admindocs',
-	'app_test',
 )
 
 # A sample logging configuration. The only tangible logging
--- app_test/views.py
+++ app_test/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render_to_response
 from django.template import Context, Template, loader
 from django.db import settings
+import socket
 
 def appserver(request, template_file="appserver.html"):
     c = Context({'result': 'succeded',})
@@ -10,9 +11,9 @@ def base(request, template_file="base.html"):
 
     return render_to_response(template_file)
 
-def serverid(request, template_file="serverid.html"):
-    c = Context({'ip_address': request.META['REMOTE_ADDR']})
-    t = Context({'hostname': 'andy',})
+def serverid(request, template_file="serverid.html",  host_name = socket.gethostname()):
+    c = Context({'ip_address': request.META['SERVER_ADDR']})
+    t = Context({'hostname': host_name})
     return render_to_response(template_file, c, t)
 
 
