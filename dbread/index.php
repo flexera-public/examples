35e3d02e98f38ea180ec155de6968fbe0c4f4ebf
Neve <n.stakanov@tut.by>
2012-09-17 10:33:56 +0300

    update to use postgrs db db connection

--- dbread/index.php
+++ dbread/index.php
@@ -1,13 +1,14 @@
+
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN"
    "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
-   
+
 <!-- # Copyright 2010 RightScale, Inc. All rights reserved.  -->
 
-   
+
 <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
 <head>
     <title>RightScale Unified Test App</title>
-    <link rel="stylesheet" type="text/css" href="../style.css" />  
+    <link rel="stylesheet" type="text/css" href="../style.css" />
 </head>
 
 <body>
@@ -24,23 +25,35 @@
 <?php
 
 include '../config/db.php';
+// @ravibhure@gmail.com
+$dbh = pg_connect("host=$hostname_DB dbname=$database_DB user=$username_DB password=$password_DB");
 
-mysql_connect($hostname_DB,$username_DB,$password_DB);
-@mysql_select_db($database_DB) or die( "Unable to select database");
-$query="SELECT * FROM app_test";
-$result=mysql_query($query);
+ if (!$dbh) {
+     die("Error in connection: " . pg_last_error());
+ }
+ 
+ // execute query
+ $sql = "SELECT * FROM app_test";
+ $result = pg_query($dbh, $sql);
+ if (!$result) {
+     die("Error in SQL query: " . pg_last_error());
+ }
+ 
+$num=pg_num_rows($result);
 
-$num=mysql_numrows($result);
+ // free memory
+ // pg_free_result($result);
 
-mysql_close();
+ // close connection
+ pg_close($dbh);
 
 echo "<b><center> Database($database_DB) Output From Host($hostname_DB)</center></b><br><br>";
 
 $i=0;
 while ($i < $num) {
 
-$name=mysql_result($result,$i,"name");
-$value=mysql_result($result,$i,"value");
+$name=pg_result($result,$i,"name");
+$value=pg_result($result,$i,"value");
 
 
 echo "<b>$name:$value</b><br><hr><br>";
@@ -55,3 +68,4 @@ $i++;
 
 </body>
 </html>
+
